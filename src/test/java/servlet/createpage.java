b6b3240dbf285910fa1fa923538058edf6566eba
my3github <45078698+my3github@users.noreply.github.com>
2018-11-17 10:38:51 +0530

    Update createpage.java

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://13.228.27.227:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://54.89.225.128:8080/postgres";
 			String username="postgres";
 			String password="";
 		
